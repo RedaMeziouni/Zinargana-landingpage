721b36f3bd2770dbc0ecd6af987704ea5ce7776b
RedaMeziouni <meziounidb@gmail.com>
2021-09-20 03:35:59 +0200

    To be continued...

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+    // Product Carousel
     $('#product-carousel').owlCarousel({
         loop:true,
         margin:0,
@@ -16,6 +16,27 @@ $(document).ready(function(){
             },
             1000:{
                 items:4
+            }
+        }
+    })
+
+    // Testimonial Carousel
+    $('#testimonial-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
             }
         }
     })
